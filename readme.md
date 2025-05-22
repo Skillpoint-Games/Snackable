28a1899e0eadd9d026795d7e36ee4384028701f2
LittleDeltaPlus <seamusrdoran@gmail.com>
2025-05-22 17:32:46 +0100

    Initial Commit

---
+++ readme.md
@@ -0,0 +1,6 @@
+﻿# Snackable
+A Simple mod that makes less saturating food quicker to eat.
+
+Consume speed is defined as: `Saturation / Nutrition * (the default consume speed of 1.6) `
+
+This may or may not be compatible with modded food.
---
+++ src/main/java/skillpoint/snackable/mixin/ItemSettingsMixin.java
@@ -0,0 +1,30 @@
+package skillpoint.snackable.mixin;
+
+import com.llamalad7.mixinextras.sugar.Local;
+import com.llamalad7.mixinextras.sugar.ref.LocalRef;
+import net.minecraft.component.type.ConsumableComponent;
+import net.minecraft.component.type.FoodComponent;
+import net.minecraft.item.Item;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(Item.Settings.class)
+public class ItemSettingsMixin {
+
+    @Inject(at = @At("HEAD"), method = "food(Lnet/minecraft/component/type/FoodComponent;Lnet/minecraft/component/type/ConsumableComponent;)Lnet/minecraft/item/Item$Settings;")
+    private void food(FoodComponent foodComponent, ConsumableComponent consumableComponent, CallbackInfoReturnable<Item.Settings> cir, @Local(argsOnly = true) LocalRef<ConsumableComponent> localRef) {
+        // use saturation to nutrition ratio to override default consume time
+        var saturationModifier = foodComponent.saturation() / foodComponent.nutrition();
+        var timeOverride = saturationModifier * ConsumableComponent.DEFAULT_CONSUME_SECONDS;
+
+        // copy consumable component replacing consume time
+        localRef.set(new ConsumableComponent(
+                timeOverride,
+                consumableComponent.useAction(),
+                consumableComponent.sound(),
+                consumableComponent.hasConsumeParticles(),
+                consumableComponent.onConsumeEffects()));
+    }
+}
